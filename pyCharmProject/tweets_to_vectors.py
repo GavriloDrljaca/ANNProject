a3ad17b33df94428f777e46e60a4dd760fb27b19
Gavrilo <Gavrilo@DESKTOP-C8F73M2>
2016-09-12 00:25:52 +0200

    Pisanje diplomskog

--- pyCharmProject/tweets_to_vectors.py
+++ pyCharmProject/tweets_to_vectors.py
@@ -2,17 +2,28 @@ import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 from gensim import corpora, models, similarities, utils
+from nltk_manipulation import *
 from twitterData import *
 from pprint import pprint  # pretty-printer
 
 
-def create_dictionary(tweets_file, dictionary_name, stoplist):
+def create_dictionary(tweets_file, dictionary_name, stoplist, stemmer_type):
+    """
+    :param tweets_file: name of the file where tweets are
+    :param dictionary_name: under this name(and path) dictionary file will be saved
+    :param stoplist: list of 'stop' words
+    :param stemmer_type: type of stemmer (lancaster, porter, snowball, regexp or none)
+    :return:
+    """
     all_tweets_file = open(tweets_file, 'r')
     all_tweets = all_tweets_file.read()
     tw = all_tweets.replace("\\n", " ")
     stop_words = load_stoplist(stoplist)
-    texts = [word for word in tw.lower().split() if word not in stop_words]
-
+    stemmer = get_stemmer(stemmer_type)
+    if(stemmer_type ==  'none'):
+        texts = [word for word in tw.lower().split() if word not in stop_words]
+    else:
+        texts = [stemmer.stem(word) for word in tw.lower().split() if word not in stop_words]
     dictionary = corpora.Dictionary([texts])
     dictionary.save(dictionary_name + '.dict')
 
@@ -44,8 +55,8 @@ X_test, y_test = create_tweets_data(["test_data/tw_auto_test",  "test_data/tw_fa
 print_tweets_to_file('./all_tweets', X_train)
 print_tweets_to_file('./all_tweets', X_test)
 
-create_dictionary('all_tweets', 'test_dict', 'stoplist.txt')
+create_dictionary('all_tweets', 'dictionaries/snowball_dict', 'stoplist.txt', 'snowball')
 dictionary = corpora.Dictionary
-my_dict = dictionary.load('test_dict.dict')
+my_dict = dictionary.load('dictionaries/snowball_dict.dict')
 print(my_dict)
 """
