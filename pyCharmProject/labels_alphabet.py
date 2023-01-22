774b601899b7f5e62122a778cabe3878da43feee
removedporn <86824510+removedporn@users.noreply.github.com>
2023-01-22 22:16:22 +0800

    Delete pyCharmProject directory

--- pyCharmProject/labels_alphabet.py
+++
@@ -1,8 +0,0 @@
-def return_label(alphabet, tag):
-    return alphabet[tag]
-
-def return_list_labels(alphabet, tag_list):
-    return_list = []
-    for tag in tag_list:
-        return_list.append(alphabet[tag])
-    return return_list
